4d69f0a0933e3d0a229cc1899ad50702f3396ac1
maxstw23 <74139226+maxstw23@users.noreply.github.com>
2026-02-26 17:43:04 -0800

    up

--- Etabins_20_Coalescence/Etabins_20_Coalescence/print_datapoints.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for energy in "7GeV" "9GeV" "11GeV" "14GeV" "17GeV" "19GeV" "27GeV"; do
-    for particle in pions kaons protons; do
-        root -b -q "PrintDatapoints.C(\"${energy}\", \"${particle}\");"
-    done
-done
-
